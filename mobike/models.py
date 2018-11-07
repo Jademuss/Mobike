6bb31eae7547d4e93d7ed36bc5f8a4696b424ec8
Jademuss <jav.guzmana@alumnos.duoc.cl>
2018-11-06 23:58:12 -0300

    Base proyecto mobike, incluyendo los models

---
+++ mobike/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Cliente(models.Model):
+    MEDIO_PAGO = (
+    ('Debito', 'debito'),
+    ('Credito', 'credito'),
+    ('TarjetaMunicipal', 'tarjetamunicipal'),
+)
+    ID_CLIENTE = models.AutoField(primary_key=True)
+    RUT = models.CharField(max_length=200)
+    NOMBRECOMPLETO = models.CharField(max_length=200)
+    EDAD  = models.IntegerField()
+    MEDIO_PAGO = models.CharField(max_length=30, choices=MEDIO_PAGO)
+    NUM_CELULAR = models.CharField(max_length=200)    
+    EMAIL = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.NOMBRECOMPLETO
+
+class Bicicleta(models.Model):
+    ID_MOBIKE = models.AutoField(primary_key=True)
+    COORDENADAS = models.CharField(max_length=200)
+    QR = models.CharField(max_length=200)
+    ESTADO_CANDADO  = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.ID_MOBIKE
+
+class Arriendo(models.Model):
+    fecha_ingreso =models.DateTimeField()
+    ID_ARRIENDO = models.AutoField(primary_key=True)
+    ID_MOBIKE = models.ForeignKey(Bicicleta, on_delete=models.CASCADE)
+    ID_CLIENTE = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    #tiempo =models.DateTimeField()
+
+    def __str__(self):
+        return self.ID_ARRIENDO
