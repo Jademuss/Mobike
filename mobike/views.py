085042ae822b317e8a994e0a78e5c010db759877
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 20:39:23 -0300

    se cambia nombre de formulario CreateUser por formularioCliente

--- mobike/views.py
+++ mobike/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 
 def inicio(request):
-    return render(request, 'mobike/inicio.html', {})      
+    return render(request, 'mobike/inicio.html', {})
 
 
 def regis_view(request,pk):
     goku = get_object_or_404(Goku, pk=pk)
     context = {'goku':goku}
-    return render (request, 'mobike/goku_details.html',context)      
+    return render (request, 'mobike/goku_details.html',context)
 
 
 
 def regis(request):
     if request.method == 'POST':
-        form = CreateUser( request.POST,request.FILES)
+        form = formularioCliente( request.POST,request.FILES)
         if form.is_valid():
             gohan = form.save(commit=False)
             gohan.save()
             return redirect('regis_view',pk=gohan.pk)
     else:
-        form = CreateUser()
+        form = formularioCliente()
     context = {'form':form}
     return render(request, 'mobike/goku_edit.html',context)
 
@@ -28,12 +28,12 @@ def regis(request):
 def regis_edit(request,pk):
     goku = get_object_or_404(Goku,pk=pk)
     if request.method == 'POST':
-        form = CreateUser(request.POST,request.FILES, instance=goku)
+        form = formularioCliente(request.POST,request.FILES, instance=goku)
         if form.is_valid():
             goku = form.save(commit=False)
             goku.save()
             return redirect('regis_view',pk=goku.pk)
     else:
-        form = CreateUser(instance = goku)
+        form = formularioCliente(instance = goku)
     context = {'form':form}
-    return render(request,'mobike/regis_edit.html',context)    
+    return render(request,'mobike/regis_edit.html',context)
