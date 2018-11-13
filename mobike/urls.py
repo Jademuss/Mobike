b1c2cc2385cf97a66ab74c61fbb66bead1ac4a5c
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 20:55:32 -0300

    agrego una url para ver detalle de cliente segun el entero de ID_CLIENTE

--- mobike/urls.py
+++ mobike/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
-    path('', views.inicio, name='inicio'),        
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('cliente/<int:ID_CLIENTE>/', views.getCliente, name='getCliente'),
     path('inicio/cli_lista/', views.cli_lista, name='cli_lista'),
     path('inicio/cli_lista/<int:pk>/', views.regis_view, name='regis_view'),
-    path('inicio/regis/', views.regis, name= 'regis),    
+    path('inicio/regis/', views.regis, name= 'regis),
     path('inicio/<int:pk>/edit/', views.regis_edit , name ='regis_edit'),
 ]
