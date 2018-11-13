68c701485c3c05cc688b4af147a38188f7579907
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 20:39:57 -0300

    se cambia el nombre de la clase del formulario

--- mobike/forms.py
+++ mobike/forms.py
@@ -3,8 +3,8 @@ from .models import Cliente
 from django.contrib.auth.models import User
 
 
-class CreateUser(forms.ModelForm):
-    
+class formularioCliente(forms.ModelForm):
+
     class Meta:
         model = Cliente
         fields = ['RUT','NOMBRE_COMPLETO','EDAD','MEDIO_PAGO','NUMERO_CELULAR','EMAIL']
