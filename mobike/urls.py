14ab1aded53a5cfa919ee1024494f1fc71c49691
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 12:13:14 -0300

    comente las url para arreglar el runsever

--- mobike/urls.py
+++ mobike/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
-    path('', views.inicio, name='inicio'),        
-    path('inicio/cli_lista/', views.cli_lista, name='cli_lista'),
-    path('inicio/cli_lista/<int:pk>/', views.regis_view, name='regis_view'),
-    path('inicio/regis/', views.regis, name= 'regis),    
-    path('inicio/<int:pk>/edit/', views.regis_edit , name ='regis_edit'),
+urlpatterns = [
+    path('', views.index, name='index'),
+    #path('', views.inicio, name='inicio'),
+    #path('inicio/cli_lista/', views.cli_lista, name='cli_lista'),
+    #path('inicio/cli_lista/<int:pk>/', views.regis_view, name='regis_view'),
+    #path('inicio/regis/', views.regis, name= 'regis),
+    #path('inicio/<int:pk>/edit/', views.regis_edit , name ='regis_edit'),
 ]
