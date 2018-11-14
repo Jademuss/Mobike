e2fd8d61c6b0e2b7b1113ddb2c6c19f935a1d1c9
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 21:13:02 -0300

    se realizaron cambios a views

--- mobike/views.py
+++ mobike/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, render_to_response
 from .models import Cliente
 
 
 
-def inicio(request):
-    return render(request, 'mobike/inicio.html', {})
+def main (request):
+    return render_to_response('mobike/main.html')
 
 
 
@@ -14,36 +15,36 @@ def getCliente(request, ID_CLIENTE):
 
 
 
-def regis_view(request,pk):
-    goku = get_object_or_404(Goku, pk=pk)
-    context = {'goku':goku}
-    return render (request, 'mobike/goku_details.html',context)
+# def regis_view(request,pk):
+#    goku = get_object_or_404(Goku, pk=pk)
+#    context = {'goku':goku}
+#    return render (request, 'mobike/goku_details.html',context)
 
 
 
-def regis(request):
-    if request.method == 'POST':
-        form = formularioCliente( request.POST,request.FILES)
-        if form.is_valid():
-            gohan = form.save(commit=False)
-            gohan.save()
-            return redirect('regis_view',pk=gohan.pk)
-    else:
-        form = formularioCliente()
-    context = {'form':form}
-    return render(request, 'mobike/goku_edit.html',context)
+# def regis(request):
+#     if request.method == 'POST':
+#         form = CreateUser( request.POST,request.FILES)
+#         if form.is_valid():
+#             gohan = form.save(commit=False)
+#             gohan.save()
+#             return redirect('regis_view',pk=gohan.pk)
+#     else:
+#         form = CreateUser()
+#     context = {'form':form}
+#     return render(request, 'mobike/goku_edit.html',context)
 
 
 
-def regis_edit(request,pk):
-    goku = get_object_or_404(Goku,pk=pk)
-    if request.method == 'POST':
-        form = formularioCliente(request.POST,request.FILES, instance=goku)
-        if form.is_valid():
-            goku = form.save(commit=False)
-            goku.save()
-            return redirect('regis_view',pk=goku.pk)
-    else:
-        form = formularioCliente(instance = goku)
-    context = {'form':form}
-    return render(request,'mobike/regis_edit.html',context)
+# def regis_edit(request,pk):
+#    goku = get_object_or_404(Goku,pk=pk)
+#    if request.method == 'POST':
+#        form = CreateUser(request.POST,request.FILES, instance=goku)
+#        if form.is_valid():
+#            goku = form.save(commit=False)
+#            goku.save()
+#            return redirect('regis_view',pk=goku.pk)
+#    else:
+#        form = CreateUser(instance = goku)
+#    context = {'form':form}
+#    return render(request,'mobike/regis_edit.html',context)
