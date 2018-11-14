dea204794fc3a7ec7885fa0f850a00534fb5bcf1
Felipe Leiva Silva <f4.leivasilva@outlook.cl>
2018-11-13 21:17:16 -0300

    cambios en url

--- mobike/urls.py
+++ mobike/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.main, name='main'),
     path('cliente/<int:ID_CLIENTE>/', views.getCliente, name='getCliente'),
-    path('inicio/cli_lista/', views.cli_lista, name='cli_lista'),
-    path('inicio/cli_lista/<int:pk>/', views.regis_view, name='regis_view'),
-    path('inicio/regis/', views.regis, name= 'regis),
-    path('inicio/<int:pk>/edit/', views.regis_edit , name ='regis_edit'),
+    #path('inicio/regis/', views.regis, name= 'regis),
+    #path('inicio/cli_lista/', views.cli_lista, name='cli_lista'),
+    #path('inicio/cli_lista/<int:pk>/', views.regis_view, name='regis_view'),
+    #path('inicio/<int:pk>/edit/', views.regis_edit , name ='regis_edit'),
 ]
